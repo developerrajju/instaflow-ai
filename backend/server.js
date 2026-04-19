e30b28b9e55c73a7ac9e5977fe57965521787319
RAJESH R <iamrajeshreddy1@gmail.com>
2026-04-19 12:33:52 +0530

    Add backend server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/instaflow", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req,res)=>res.send("API Running"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
