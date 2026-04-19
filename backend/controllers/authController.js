788757ced1637d9b732b9f5f5b19f9dddf4b49e0
RAJESH R <iamrajeshreddy1@gmail.com>
2026-04-19 12:58:00 +0530

    edt

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,27 +1,57 @@
 const User = require("../models/User");
 const generateToken = require("../utils/generateToken");
+const { hashPassword, matchPassword } = require("../utils/hashPassword");
 
+// REGISTER
 exports.register = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.create({ email, password });
-  res.json({
-    _id: user._id,
-    email: user.email,
-    token: generateToken(user._id),
-  });
+try {
+const { email, password } = req.body;
+
+```
+const existingUser = await User.findOne({ email });
+if (existingUser) {
+  return res.status(400).json({ message: "User already exists" });
+}
+
+const hashedPassword = await hashPassword(password);
+
+const user = await User.create({
+  email,
+  password: hashedPassword,
+});
+
+res.json({
+  _id: user._id,
+  email: user.email,
+  token: generateToken(user._id),
+});
+```
+
+} catch (err) {
+res.status(500).json({ message: err.message });
+}
 };
 
+// LOGIN
 exports.login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
+try {
+const { email, password } = req.body;
 
-  if (!user) {
-    return res.status(400).json({ message: "Invalid credentials" });
-  }
+```
+const user = await User.findOne({ email });
 
-  res.json({
-    _id: user._id,
-    email: user.email,
-    token: generateToken(user._id),
-  });
+if (!user || !(await matchPassword(password, user.password))) {
+  return res.status(400).json({ message: "Invalid credentials" });
+}
+
+res.json({
+  _id: user._id,
+  email: user.email,
+  token: generateToken(user._id),
+});
+```
+
+} catch (err) {
+res.status(500).json({ message: err.message });
+}
 };
